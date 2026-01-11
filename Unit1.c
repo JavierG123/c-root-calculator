24529650c6737d37af27af781e2ac520ef5841f5
Javier Gil <javiergonzalog22@gmail.com>
2026-01-11 14:37:54 +0100

    Refactor to port to vscode and win11
    Refactor main function and add screen clearing feature.

--- Unit1.c
+++ Unit1.c
@@ -1,43 +1,67 @@
 #include <stdio.h>
 #include <string.h>
-#include <conio.h>
 #include <math.h>
 
-float calc (float *z,float *o)
+float calc(float *z, float *o)
 {
-float y,x,g;
-g=(*o*3.1415926535897932384)/180;
-y=pow(pow(*z,5)*(1/cos(g))/((3-2* *z)*pow(*z,1.0/3)),2);
-return y;
-}
-void main ()
-{
-float F,z,o;
-char opc;
-do{
-  clrscr();
-  gotoxy(10,10);
-  printf("A- ingresar valores de la raiz a calcular");
-  gotoxy(10,12);
-  printf("B- Ver resultado de la raiz");
-  opc=getch();
-  clrscr();
-  switch(opc)
-  {
-  case'A':
-  printf("ingrese el valor de Z\n");
-  scanf("%f",&z);
-  clrscr();
-  printf("ingrese el valor de tita\n");
-  scanf("%f",&o);
-  F=calc(&z,&o);
-  getch();
-  break;
-  case 'B':
-  printf ("El valor de Y es %.2f",F);
-  getch();
-  break;
-  }
-}while(opc!='C');
+    float y, g;
+    g = (*o * 3.1415926535897932384) / 180;
+    y = pow(pow(*z,5)*(1/cos(g))/((3 - 2 * *z) * pow(*z,1.0/3)),2);
+    return y;
 }
 
+// Función para limpiar pantalla usando ANSI escape codes
+void limpiarPantalla() {
+    printf("\033[2J\033[H"); // limpia pantalla y mueve cursor a 0,0
+}
+
+int main()
+{
+    float F = 0, z = 0, o = 0;
+    char opc;
+    do {
+        limpiarPantalla();
+        printf("\n          Menu\n");
+        printf("A- Ingresar valores de la raiz a calcular\n");
+        printf("B- Ver resultado de la raiz\n");
+        printf("C- Salir\n");
+        printf("\nSeleccione una opcion: ");
+        opc = getchar();
+        while(getchar() != '\n'); // limpiar buffer
+
+        limpiarPantalla();
+        switch(opc)
+        {
+            case 'A':
+            case 'a':
+                printf("Ingrese el valor de Z: ");
+                scanf("%f", &z);
+                printf("Ingrese el valor de tita: ");
+                scanf("%f", &o);
+                F = calc(&z, &o);
+                printf("\nCalculo realizado. Presione Enter para continuar...");
+                while(getchar() != '\n'); // limpiar buffer
+                getchar();
+                break;
+
+            case 'B':
+            case 'b':
+                printf("El valor de Y es %.2f\n", F);
+                printf("\nPresione Enter para continuar...");
+                getchar();
+                break;
+
+            case 'C':
+            case 'c':
+                printf("Saliendo...\n");
+                break;
+
+            default:
+                printf("Opcion invalida. Presione Enter para continuar...");
+                getchar();
+        }
+
+    } while(opc != 'C' && opc != 'c');
+
+    return 0;
+}
